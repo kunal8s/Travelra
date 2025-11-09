27fbdacee03f2250e218414c31ecece4e49ad849
user1 <kunalsharmakunu09@gmail.com>
2025-11-09 19:43:50 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+// Loads environment variables from a .env file into process.env   Instead of hardcoding sensitive data (like passwords or API keys), you store them in a .env file.
+dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// deafult browser block request coming from different origin(domain,port or protocol) but cors help to overcome this  later modify it for particular domain
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Hello world');
+})
+
+module.exports = app;
